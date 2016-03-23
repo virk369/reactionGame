e607807f8bd71ca4ee92004040c3d8071b08ae43
virk369 <virk369@gmail.com>
2016-03-22 22:05:02 -0700

    ui commit

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ c=0;
 ad=0;
 da=0;
 setTimeout("spel()",1000+1000*rnd());
-document.ful.text8.value="wait...";}
+document.ful.text13.value="wait...";}
 
 function stop() {
 if(da==1){return}
@@ -28,7 +28,7 @@ tidpo=(sltid-stid)/1000
 if (ad==1)tidpo=2;
 tidtot=tidtot+tidpo;
 as="";if (tidtot<1)as="0"
-document.ful.text6.value="Total time: "+as+Math.round(tidtot*100)/100+" seconds"
+document.ful.text11.value="Total time: "+as+Math.round(tidtot*100)/100+" seconds"
 as="";if (tidpo<1)as="0"
 if(ad==0){
 if (k==1)document.ful.text1.value="Reaction time 1: "+as+tidpo+" seconds"
@@ -36,15 +36,20 @@ if (k==2)document.ful.text2.value="Reaction time 2: "+as+tidpo+" seconds"
 if (k==3)document.ful.text3.value="Reaction time 3: "+as+tidpo+" seconds"
 if (k==4)document.ful.text4.value="Reaction time 4: "+as+tidpo+" seconds"
 if (k==5)document.ful.text5.value="Reaction time 5: "+as+tidpo+" seconds"
-document.ful.text8.value="Push Start";
+if (k==6)document.ful.text6.value="Reaction time 6: "+as+tidpo+" seconds"
+if (k==7)document.ful.text7.value="Reaction time 7: "+as+tidpo+" seconds"
+if (k==8)document.ful.text8.value="Reaction time 8: "+as+tidpo+" seconds"
+if (k==9)document.ful.text9.value="Reaction time 9: "+as+tidpo+" seconds"
+if (k==10)document.ful.text10.value="Reaction time 10: "+as+tidpo+" seconds"
+document.ful.text13.value="Push Start";
 }
 as="";if (tidtot<1)as="0"
-document.ful.text6.value="Total time: "+as+Math.round(tidtot*100)/100+" seconds"
+document.ful.text11.value="Total time: "+as+Math.round(tidtot*100)/100+" seconds"
 as="";if (tidtot/k<1)as="0"
-document.ful.text7.value="Avg. time: "+as+Math.round(tidtot*10000/(k*100))/100+" seconds"
+document.ful.text12.value="Avg. time: "+as+Math.round(tidtot*10000/(k*100))/100+" seconds"
 if(k>4){
 q=1
-document.ful.text8.value="Game Over...";
+document.ful.text13.value="Game Over...";
    }
 }
 
@@ -54,11 +59,11 @@ k=k+1
 c=1;
 startTime=new Date();
 stid=startTime.getTime();
-document.ful.text8.value="PUSH STOP!!!";
+document.ful.text13.value="PUSH STOP!!!";
 }
 
 function fusk() {
-document.ful.text8.value="Too soon. 2 sec penalty. Push start!"
+document.ful.text13.value="Too soon. 2 sec penalty. Push start!"
 ad=1
 stid=0;
 rak();
